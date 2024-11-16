34bf45a083a02b31b8f49c938b9c3da221ed8ae9
X-Network12 <Shivmis165@gmail.com>
2024-11-16 15:30:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,12 +52,12 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = getenv("PLAYLIST_FETCH_LIMIT", 25)
 
 
 # Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+TG_AUDIO_FILESIZE_LIMIT = getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600)
+TG_VIDEO_FILESIZE_LIMIT = getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824)
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
